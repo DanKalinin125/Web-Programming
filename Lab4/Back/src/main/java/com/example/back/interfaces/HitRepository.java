f99237a2613b888dae74416f8c4194634c524f46
DanKalinin125 <dan_kalinin_03@mail.ru>
2023-03-07 11:21:48 +0300

    Delete Lab4 directory

--- Lab4/Back/src/main/java/com/example/back/interfaces/HitRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.back.interfaces;
-
-import com.example.back.entities.Hit;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HitRepository extends CrudRepository<Hit, Long> {
-}
